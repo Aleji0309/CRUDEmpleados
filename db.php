c1161fb989b69893f0f687bf6325a9c26c09c3e4
Aleji0309 <78771441+Aleji0309@users.noreply.github.com>
2025-04-10 18:01:44 -0600

    First Commit

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+    // Definir los datos de conexión 
+
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $clave = '';
+    $basedatos = 'empleados_app';
+
+    // Crear la conexión con la base de datos 
+    $conexion =  new mysqli($servidor, $usuario, $clave, $basedatos) ;
+
+    // Mensaje de error si la conexi+pn falla
+    if($conexion->connect_error){
+        echo "Error al conectar base de datos: " , $conexion -> connect_error ;
+    } else {
+        echo "Conexión Exitosa";
+    }
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include 'db.php' ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Empleados</title>
+</head>
+<body>
+    <h1>Empleados Registrados</h1>
+
+    <table border='1' cellpadding = "10" >
+        <tr>
+            <th>ID</th>
+            <th>Clave</th>
+            <th>Nombre</th>
+            <th>Apellido Materno</th>
+            <th>Apellido Paterno</th>
+            <th>Puesto</th>
+            <th>Ingreso</th>
+            <th>Baja</th>
+            <th>Status</th>
+        </tr>
+    </table>
+</body>
+</html>
